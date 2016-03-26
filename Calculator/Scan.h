dd4c54b9c6576508b70189d5a77b1372e206f83b
424baopu <13055721810@qq.com>
2016-03-26 22:06:03 +0800

    Update Scan.h

--- Calculator/Scan.h
+++ Calculator/Scan.h
@@ -9,10 +9,10 @@ public:
 	void ToStringQueue(string input);
 	queue<string> *GetQueue();
 	int m_ijudge = 0;
-	
+
 	Scan();
 	~Scan();
 private:
-	queue<string>*m_que = new queue<string>;//¶ÓÁÐÖ¸Õë
+	queue<string>*m_que = new queue<string>;
 };
 
